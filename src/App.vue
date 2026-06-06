b2fe5b4bfe927c656f8ab021a6a68ab2652eb5c7
Mizuki <mm329729@gmail.com>
2026-06-07 08:25:53 +0900

    feat: props を定義

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { ref } from 'vue'
 import ChildComp from './components/ChildComp.vue'
+
+const greeting = ref('Hello from parent')
 </script>
 
 <template>
-  <ChildComp />
+  <ChildComp :msg="greeting" />
 </template>
