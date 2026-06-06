f5adfecc59957b4c4575d6c42dc21ba24013a950
Mizuki <mm329729@gmail.com>
2026-06-07 05:40:01 +0900

    feat: v-forによる配列レンダリング

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <script setup>
 import { ref } from 'vue'
 
-const awesome = ref(true)
+// give each todo a unique id
+let id = 0
 
-function toggle() {
-  awesome.value = !awesome.value
+const newTodo = ref('')
+const todos = ref([
+  { id: id++, text: 'Learn HTML' },
+  { id: id++, text: 'Learn JavaScript' },
+  { id: id++, text: 'Learn Vue' },
+])
+
+function addTodo() {
+  todos.value.push({ id: id++, text: newTodo.value })
+  newTodo.value = ''
+}
+
+function removeTodo(todo) {
+  todos.value = todos.value.filter((t) => t !== todo)
 }
 </script>
 
 <template>
-  <button @click="toggle">Toggle</button>
-  <h1 v-if="awesome">Vue is awesome!</h1>
-  <h1 v-else>Oh no 😢</h1>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" required placeholder="new todo" />
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      {{ todo.text }}
+      <button @click="removeTodo(todo)">X</button>
+    </li>
+  </ul>
 </template>
